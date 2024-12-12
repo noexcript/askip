1c3e841f8b53d3ced53fb6e533ed1c7b55d4e197
noexcript <noexcript@gmail.com>
2024-12-12 22:55:34 +0100

    feat: added titulo sobre ganhar

--- src/components/homeText.jsx
+++ src/components/homeText.jsx
@@ -5,7 +5,7 @@ export default function HomeText() {
         <div className="container flex flex-col gap-8 md:gap-4 w-full lg:w-[75%] text-white ">
 
             <p className="text-xl md:text-2xl lg:text-5xl  leading-[40px] lg:leading-[70px] font-party text-center font-extrabold roboto-black ">
-                Para poder participar, voce precisa preencher os pré-requisitos: Increveve-se ao canal youtube, like e comenta no último video, siga ASKIP no instagram.
+            Ganhe até €1500,00 
             </p>
 
 
