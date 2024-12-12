28ac40527d96e915656a44daa2dda6835d760826
noexcript <noexcript@gmail.com>
2024-12-12 22:59:42 +0100

    feat: added titulo sobre ganhar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ function Header() {
     return (
         <header className="w-full bg-transparent flex 
          items-center justify-center  px-6">
-        <div className="w-full flex items-start justify-between px-0 ">
+        <div className="w-full flex items-start justify-between px-0 mt-2">
           <Logo />
           <Menu />
         </div>
