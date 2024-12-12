0b36f7148d3f7f7e203fffde56fcc8d2e15f5cfd
noexcript <noexcript@gmail.com>
2024-12-12 08:05:44 +0100

    feat: adde askip as base url

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/askip/',  
+  plugins: [react()], 
   build: {
     outDir: 'dist',  
   },
