6bca32c259c662ec49c78df81cbd02beea71e5fd
noexcript <noexcript@gmail.com>
2024-12-18 22:38:02 +0100

    feat:troca de logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import Menu from "./menu"
 function Header() {
     return (
         <header className="w-full bg-transparent flex 
-         items-center justify-center  px-6">
+         items-center justify-center  px-6 -mt-4">
         <div className="w-full flex items-center justify-between px-0">
           <Logo />
           <Menu />
--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -5,10 +5,9 @@ export default function Logo() {
         <h1>
             <Link
                 to="/"
-                className="font-rubik  font-extrabold italic animate-gradient flex items-center  p-0"
+                className="font-rubik  font-extrabold italic animate-gradient flex items-center  p-0 "
             >
-                <img className="w-[60px] h-[60px] md:w-[80px] md:h-[80px] object-cover " src="/img/logo.png" alt="askip-logo" />
-                <img className="w-[80px] h-[80px] md:w-[120px] md:h-[120px] object-cover " src="/img/2.png" alt="askip-logo" />
+                <img className="w-[100px] h-[90px] md:w-[140px] md:h-[100px] object-contain " src="/img/logo.png" alt="askip-logo" />
             </Link>
         </h1>
 
