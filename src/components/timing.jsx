796adfe1d163e321a9f6428f13157803d535455c
noexcript <noexcript@gmail.com>
2024-12-23 18:00:45 +0100

    feat: better at timing

--- src/components/timing.jsx
+++ src/components/timing.jsx
@@ -15,7 +15,7 @@ export default function Timing() {
         <div className="w-full bg-slate-950">
             <div className="gap-2 text-sm font-bold flex items-center justify-center text-blue-400 p-3  ">
 
-                {(participant.clicked && isTiming) ? Object.entries(timesLeftTip).map(([key, value], index) => (
+                {/* {(participant.clicked && isTiming) ? Object.entries(timesLeftTip).map(([key, value], index) => (
                     <div className="flex flex-row items-center justify-center roboto-medium border p-2 w-[40px] h-[30px] rounded-sm border-blue-400" key={index}>
                         <span>{value}</span>
                         <span className="hidden md:flex">{key}</span>
@@ -27,7 +27,13 @@ export default function Timing() {
                             <span className="hidden md:flex">{key}</span>
                         </div>
                     ))
-                }
+                } */}
+                {Object.entries(timesLeftTip).map(([key, value], index) => (
+                    <div className="flex flex-row items-center justify-center roboto-medium border p-2 w-[40px] h-[30px] rounded-sm border-blue-400" key={index}>
+                        <span>{value}</span>
+                        <span className="hidden md:flex">{key}</span>
+                    </div>
+                ))}
 
             </div>
         </div>
